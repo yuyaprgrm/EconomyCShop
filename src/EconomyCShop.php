f53378092107010d0c3eb8a39e7fba980f87bb9d
famima65536 <famima65536@gmail.com>
2021-12-31 14:10:28 +0900

    use src-namespace-prefix and relocate.

---
+++ src/EconomyCShop.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace famima65536\EconomyCShop;
+
+use pocketmine\plugin\PluginBase;
+
+class EconomyCShop extends PluginBase{
+}
---
+++ src/EventListener.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace famima65536\EconomyCShop;
+
+use pocketmine\event\Listener;
+
+class EventListener implements Listener{
+}
