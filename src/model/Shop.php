12605d52d8e5e3e0dd33ed571b586dc60ceb5133
famima65536 <famima65536@gmail.com>
2021-12-31 14:44:31 +0900

    write shop model

--- src/model/Shop.php
+++ src/model/Shop.php
@@ -5,6 +5,53 @@ namespace famima65536\EconomyCShop\model;
 
 class Shop {
 	public function __construct(
+		private string $owner,
+		private Product $product,
+		private Coordinate $signCoordinate,
+		private Coordinate $mainChest,
+		private ?Coordinate $subChest=null
 	){
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getOwner(): string{
+		return $this->owner;
+	}
+
+	/**
+	 * @return Product
+	 */
+	public function getProduct(): Product{
+		return $this->product;
+	}
+
+	/**
+	 * @return Coordinate
+	 */
+	public function getSignCoordinate(): Coordinate{
+		return $this->signCoordinate;
+	}
+
+	/**
+	 * @return Coordinate
+	 */
+	public function getMainChest(): Coordinate{
+		return $this->mainChest;
+	}
+
+	/**
+	 * @return Coordinate|null
+	 */
+	public function getSubChest(): ?Coordinate{
+		return $this->subChest;
+	}
+
+	/**
+	 * @param Coordinate|null $subChest
+	 */
+	public function setSubChest(?Coordinate $subChest): void{
+		$this->subChest = $subChest;
 	}
 }
