3bf02209e2bbed8fb7fc60ada3a99c591ad20fda
famima65536 <famima65536@gmail.com>
2021-12-31 17:01:04 +0900

    add getHash method

--- src/model/Coordinate.php
+++ src/model/Coordinate.php
@@ -2,7 +2,9 @@
 
 namespace famima65536\EconomyCShop\model;
 
-class Coordinate {
+use pocketmine\world\World;
+
+class Coordinate implements \JsonSerializable {
 	public function __construct(
 		private int $x,
 		private int $y,
@@ -29,5 +31,17 @@ class Coordinate {
 	 */
 	public function getZ(): int{
 		return $this->z;
+	}
+
+	public function jsonSerialize(): array{
+		return [
+			"x" => $this->x,
+			"y" => $this->y,
+			"z" => $this->z
+		];
+	}
+
+	public function getHash(): int{
+		return World::blockHash($this->x, $this->y, $this->z);
 	}
 }
