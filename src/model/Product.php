3a6cec5c66ae238973fbd705c420ca7316d1ef08
famima65536 <famima65536@gmail.com>
2021-12-31 15:16:04 +0900

    add product validation logic.

--- src/model/Product.php
+++ src/model/Product.php
@@ -2,14 +2,23 @@
 
 namespace famima65536\EconomyCShop\model;
 
+
+use famima65536\EconomyCShop\model\exception\InvalidProductException;
+use InvalidArgumentException;
 use pocketmine\item\Item;
 
 class Product {
 	public function __construct(
 		private Item $item,
-		private int $price,
-		private int $amount
+		private int $price
 	){
+		if($this->price < 0){
+			throw new InvalidProductException("Price should be equal or greater than zero", InvalidProductException::INVALID_PRICE);
+		}
+
+		if($this->item->getCount() <= 0){
+			throw new InvalidProductException("Count should be greater than zero.", InvalidProductException::INVALID_AMOUNT);
+		}
 	}
 
 	/**
@@ -30,6 +39,6 @@ class Product {
 	 * @return int
 	 */
 	public function getAmount(): int{
-		return $this->amount;
+		return $this->item->getCount();
 	}
 }
