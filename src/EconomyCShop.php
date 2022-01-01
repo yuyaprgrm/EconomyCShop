451e4c9a4a0a591d1334a314c93934e7c8e4dcae
famima65536 <famima65536@gmail.com>
2022-01-01 14:22:20 +0900

    prepare for Sqlite repository.

--- src/EconomyCShop.php
+++ src/EconomyCShop.php
@@ -5,10 +5,12 @@ namespace famima65536\EconomyCShop;
 use famima65536\EconomyCShop\application\ShopApplicationService;
 use famima65536\EconomyCShop\repository\IShopRepository;
 use famima65536\EconomyCShop\repository\JsonShopRepository;
+use famima65536\EconomyCShop\repository\SqliteShopRepository;
 use famima65536\EconomyCShop\service\ShopService;
 use famima65536\EconomyCShop\utils\economy\EconomyAPIWrapper;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use SQLite3;
 use Webmozart\PathUtil\Path;
 
 class EconomyCShop extends PluginBase{
@@ -16,12 +18,11 @@ class EconomyCShop extends PluginBase{
 	private IShopRepository $shopRepository;
 
 	public function onLoad(): void{
-		$shopDataConfig = new Config(Path::join($this->getDataFolder(), "shops.json"), Config::JSON);
-		$this->shopRepository = new JsonShopRepository($shopDataConfig);
+		$this->saveDefaultConfig();
+		$this->saveResource("message.yml");
 	}
 
 	public function onEnable(): void{
-		$this->saveResource("message.yml");
 		$shopService = new ShopService($this->shopRepository);
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener(
 			new ShopApplicationService($shopService, $this->shopRepository),
@@ -30,6 +31,25 @@ class EconomyCShop extends PluginBase{
 			new EconomyAPIWrapper(),
 			new Config(Path::join($this->getDataFolder(), "message.yml"), Config::YAML)
 		), $this);
+	}
+
+	private function setupRepository(){
+
+		$selectedRepository = "";
+		switch($this->getConfig()->get("repository")){
+			default:
+				$this->getLogger()->error("Unknown repository type is selected!");
+			case "json":
+				$this->shopRepository = new JsonShopRepository(new Config(Path::join($this->getDataFolder(), "shops.json")));
+				$selectedRepository = "json";
+				break;
+//			case "sqlite":
+//				$this->shopRepository = new SqliteShopRepository(new SQLite3(Path::join($this->getDataFolder(), "shops.sqlite")));
+//				$selectedRepository = "sqlite";
+//				break;
+		}
+
+		$this->getLogger()->info("${selectedRepository} is selected to save shop data");
 	}
 
 
